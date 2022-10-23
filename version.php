0694bee76f81f47be1d8beecc4c75709b6b32ec3
Aadarsha Dhakal <braveboy2058@gmail.com>
2022-10-23 14:45:54 +0545

    Version upgrade

--- version.php
+++ version.php
@@ -26,6 +26,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022052201;       // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2022052202;       // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022041300;    // Requires this Moodle version.
 $plugin->component = 'local_chature';        // Full name of the plugin (used for diagnostics)
